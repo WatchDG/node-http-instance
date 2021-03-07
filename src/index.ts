0f05255ff3714c304edfcdb50c8cd7ee20fa07e1
WatchDG <baranov@w3m.co>
2021-03-07 14:53:05 +0300

    make requests via http-instance-request

--- src/index.ts
+++ src/index.ts
@@ -1,201 +1,179 @@
 import * as http from 'http';
 import * as https from 'https';
-import { URL } from 'url';
+import {URL} from 'url';
 
-import { ResultOk, ResultFail, ReturningResultAsync } from 'node-result';
+import {ResultOk, ReturningResultAsync} from 'node-result';
+
+import {request, RequestResponse, HttpOptions} from "http-instance-request";
+
 
 type HttpInstanceOptions = {
-  baseUrl: string;
-  timeout?: number;
-  headers?: { [key: string]: string };
-};
-
-type HttpRequestData = { [Key: string]: unknown } | string;
-
-type HttpRequestOptions = {
-  url: URL;
-  options: http.RequestOptions | https.RequestOptions;
-  data?: HttpRequestData;
+    baseUrl: string;
+    timeout?: number;
+    headers?: { [key: string]: string };
 };
 
 type HttpResponse<D> = {
-  status: number;
-  headers: http.IncomingHttpHeaders;
-  data?: D;
+    status: number;
+    headers: http.IncomingHttpHeaders;
+    data?: D;
 };
 
 type HttpMethodOptions = {
-  headers?: { [key: string]: string };
-  params?: { [key: string]: string };
+    headers?: { [key: string]: string };
+    params?: { [key: string]: string };
+};
+
+type UrlObj = {
+    baseUrl: URL,
+    path: string
+};
+
+type OptionObj = {
+    method: 'GET' | 'DELETE' | 'POST' | 'PUT';
+    options: HttpOptions;
+    headers?: { [key: string]: string };
+    params?: { [key: string]: string };
 };
 
 export class HttpInstance {
-  private readonly url: URL;
-  private readonly options: (http.RequestOptions | https.RequestOptions) & { headers: { [Key: string]: string } };
-  private readonly timeout: number;
+    private readonly url: URL;
+    private readonly options: (http.RequestOptions | https.RequestOptions) & { headers: { [Key: string]: string } };
+    private readonly timeout: number;
 
-  constructor(options: HttpInstanceOptions) {
-    this.url = new URL(options.baseUrl);
-    this.options = {
-      headers: Object.assign(
-        {
-          Accept: 'application/json'
-        },
-        options.headers
-      )
-    };
-    this.timeout = options.timeout ?? 1000;
-  }
+    constructor(options: HttpInstanceOptions) {
+        this.url = new URL(options.baseUrl);
+        this.options = {
+            headers: Object.assign({
+                    Accept: 'application/json'
+                },
+                options.headers
+            )
+        };
+        this.timeout = options.timeout ?? 1000;
+    }
 
-  private request<D>(options: HttpRequestOptions): ReturningResultAsync<HttpResponse<D>, Error> {
-    const url = options.url;
-    const httpRequest = url.protocol === 'http:' ? http.request : https.request;
-    return new Promise((resolve) => {
-      try {
-        const request = httpRequest(url, options.options, (response) => {
-          const status = response.statusCode;
-          if (status) {
-            if (status < 200 || status >= 300) resolve(ResultFail(new Error(`Non success status code. ${status}`)));
-            const headers = response.headers;
-            const contentType = headers['content-type'];
-            if (contentType) {
-              if (contentType.includes('application/json')) {
-                let buffer = Buffer.alloc(0);
-                response.on(
-                  'data',
-                  (chunk) =>
-                    (buffer = Buffer.concat([buffer, chunk], Buffer.byteLength(buffer) + Buffer.byteLength(chunk)))
-                );
-                response.on('end', () => {
-                  resolve(
-                    ResultOk({
-                      status,
-                      headers,
-                      data: JSON.parse(buffer.toString())
-                    })
-                  );
-                });
-              } else {
-                resolve(ResultFail(new TypeError('Unsupported content type.')));
-              }
-            } else {
-              resolve(
-                ResultOk<HttpResponse<D>>({
-                  status,
-                  headers
-                })
-              );
+    private static prepareUrlAndOptions(urlObj: UrlObj, optionObj: OptionObj, body?: unknown): {
+        url: URL,
+        options: HttpOptions,
+        body: unknown
+    } {
+        const {baseUrl, path} = urlObj;
+        const url = new URL(path, baseUrl);
+        const {method, options: httpOptions, params, headers,} = optionObj;
+        const options = Object.assign({method}, httpOptions);
+        if (params) for (const param of Object.keys(params)) url.searchParams.append(param, params[param]);
+        if (headers) Object.assign(options.headers, headers);
+        return {url, options, body};
+    }
+
+    private static prepareBody(data?: Record<string, unknown> | string) {
+        if (typeof data === 'object') {
+            const body = JSON.stringify(data);
+            const contentType = 'application/json';
+            const contentLength = Buffer.byteLength(body);
+            return {
+                headers: {
+                    'content-type': contentType,
+                    'content-length': contentLength
+                },
+                body
             }
-          } else {
-            resolve(ResultFail(new Error('Unknown status code.')));
-          }
+        }
+        if (typeof data === 'string') {
+            const body = data;
+            const contentType = 'text/plain';
+            const contentLength = Buffer.byteLength(body);
+            return {
+                headers: {
+                    'content-type': contentType,
+                    'content-length': contentLength
+                },
+                body
+            }
+        }
+        return {headers: {}};
+    }
+
+    private static bodyToData<D>(requestResponse: RequestResponse): HttpResponse<D> {
+        const {status, headers, body} = requestResponse;
+        const contentType = headers['content-type'];
+        if (body) {
+            switch (contentType) {
+                case 'application/json':
+                    return {status, headers, data: JSON.parse(body.toString())};
+                case 'html/text':
+                    return {status, headers, data: body.toString() as unknown as D};
+                default:
+                    return {status, headers};
+            }
+        }
+        return {status, headers};
+    }
+
+    async get<D>(path: string, options?: HttpMethodOptions): ReturningResultAsync<HttpResponse<D>, Error> {
+        const urlAndOptions = HttpInstance.prepareUrlAndOptions({
+            baseUrl: this.url,
+            path
+        }, {
+            method: "GET",
+            options: this.options,
+            headers: options?.headers,
+            params: options?.params
         });
-        request.on('error', (error) => resolve(ResultFail(error)));
-        request.setTimeout(this.timeout);
-        if (typeof options.data === 'object') {
-          const data = JSON.stringify(options.data);
-          const dataLength = Buffer.byteLength(data);
-          request.setHeader('Content-Type', 'application/json');
-          request.setHeader('Content-Length', dataLength);
-          request.write(data);
-        }
-        if (typeof options.data === 'string') {
-          const dataLength = Buffer.byteLength(options.data);
-          const contentType = this.options.headers['Content-Type'] ?? 'text/plain';
-          request.setHeader('Content-Type', contentType);
-          request.setHeader('Content-Length', dataLength);
-          request.write(options.data);
-        }
-        request.end();
-      } catch (error) {
-        resolve(ResultFail(error));
-      }
-    });
-  }
-
-  get<D>(path: string, options?: HttpMethodOptions): ReturningResultAsync<HttpResponse<D>, Error> {
-    const url = new URL(path, this.url);
-    const requestOptions = Object.assign({}, this.options, { method: 'GET' });
-    if (options) {
-      if (options.params) {
-        for (const param of Object.keys(options.params)) {
-          url.searchParams.append(param, options.params[param]);
-        }
-      }
-      if (options.headers) {
-        Object.assign(requestOptions.headers, options.headers);
-      }
+        const requestResponse = (await request(urlAndOptions)).unwrap();
+        return ResultOk(HttpInstance.bodyToData<D>(requestResponse));
     }
-    return this.request<D>({
-      url,
-      options: requestOptions
-    });
-  }
 
-  delete<D>(path: string, options?: HttpMethodOptions): ReturningResultAsync<HttpResponse<D>, Error> {
-    const url = new URL(path, this.url);
-    const requestOptions = Object.assign({}, this.options, { method: 'DELETE' });
-    if (options) {
-      if (options.params) {
-        for (const param of Object.keys(options.params)) {
-          url.searchParams.append(param, options.params[param]);
-        }
-      }
-      if (options.headers) {
-        Object.assign(requestOptions.headers, options.headers);
-      }
+    async delete<D>(path: string, options?: HttpMethodOptions): ReturningResultAsync<HttpResponse<D>, Error> {
+        const urlAndOptions = HttpInstance.prepareUrlAndOptions({
+            baseUrl: this.url,
+            path
+        }, {
+            method: "DELETE",
+            options: this.options,
+            headers: options?.headers,
+            params: options?.params
+        });
+        const requestResponse = (await request(urlAndOptions)).unwrap();
+        return ResultOk(HttpInstance.bodyToData<D>(requestResponse));
     }
-    return this.request<D>({
-      url,
-      options: requestOptions
-    });
-  }
 
-  post<D>(
-    path: string,
-    data?: HttpRequestData,
-    options?: HttpMethodOptions
-  ): ReturningResultAsync<HttpResponse<D>, Error> {
-    const url = new URL(path, this.url);
-    const requestOptions = Object.assign({}, this.options, { method: 'POST' });
-    if (options) {
-      if (options.params) {
-        for (const param of Object.keys(options.params)) {
-          url.searchParams.append(param, options.params[param]);
-        }
-      }
-      if (options.headers) {
-        Object.assign(requestOptions.headers, options.headers);
-      }
+    async post<D>(
+        path: string,
+        data?: Record<string, unknown> | string,
+        options?: HttpMethodOptions
+    ): ReturningResultAsync<HttpResponse<D>, Error> {
+        const {headers, body} = HttpInstance.prepareBody(data);
+        const urlAndOptions = HttpInstance.prepareUrlAndOptions({
+            baseUrl: this.url,
+            path
+        }, {
+            method: "POST",
+            options: this.options,
+            headers: Object.assign(headers, options?.headers),
+            params: options?.params
+        }, body);
+        const requestResponse = (await request(urlAndOptions)).unwrap();
+        return ResultOk(HttpInstance.bodyToData<D>(requestResponse));
     }
-    return this.request<D>({
-      url,
-      options: requestOptions,
-      data
-    });
-  }
 
-  put<D>(
-    path: string,
-    data?: HttpRequestData,
-    options?: HttpMethodOptions
-  ): ReturningResultAsync<HttpResponse<D>, Error> {
-    const url = new URL(path, this.url);
-    const requestOptions = Object.assign({}, this.options, { method: 'PUT' });
-    if (options) {
-      if (options.params) {
-        for (const param of Object.keys(options.params)) {
-          url.searchParams.append(param, options.params[param]);
-        }
-      }
-      if (options.headers) {
-        Object.assign(requestOptions.headers, options.headers);
-      }
+    async put<D>(
+        path: string,
+        data?: Record<string, unknown> | string,
+        options?: HttpMethodOptions
+    ): ReturningResultAsync<HttpResponse<D>, Error> {
+        const {headers, body} = HttpInstance.prepareBody(data);
+        const urlAndOptions = HttpInstance.prepareUrlAndOptions({
+            baseUrl: this.url,
+            path
+        }, {
+            method: "PUT",
+            options: this.options,
+            headers: Object.assign(headers, options?.headers),
+            params: options?.params
+        }, body);
+        const requestResponse = (await request(urlAndOptions)).unwrap();
+        return ResultOk(HttpInstance.bodyToData<D>(requestResponse));
     }
-    return this.request<D>({
-      url,
-      options: requestOptions,
-      data
-    });
-  }
 }
